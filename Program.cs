e7c2ece5f5c95dacaf2ced8296815b53907ad01e
TEO BENJAMIN MORENO <50291929@ort>
2026-09-08 08:05:19 -0300

    session configurada

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSession();
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
-
+app.UseSession();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
